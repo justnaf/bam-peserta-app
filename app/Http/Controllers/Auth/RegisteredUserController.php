86f6f7b6a73a0f8e62f0709092cbe5da0bd1f8f1
justnaf <naufalaf86@gmail.com>
2025-01-26 22:33:53 +0700

    Modify To Make No Duplicate NPM

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,7 +31,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'username' => ['required', 'digits:10', 'max:255'],
+            'username' => ['required', 'digits:10', 'max:255', 'unique:users,username'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
