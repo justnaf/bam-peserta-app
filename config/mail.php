75702b43fc74bad869ff1aa75ca3f60b00881c81
justnaf <naufalaf86@gmail.com>
2025-01-26 21:50:10 +0700

    Restore

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,6 @@ return [
             'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
-            'encryption' => env('MAIL_ENCRYPTION', null),
             'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
