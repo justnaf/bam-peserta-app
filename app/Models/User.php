85c761b06840dedcf8aad9f49c96740d749700f2
justnaf <naufalaf86@gmail.com>
2025-01-26 20:43:11 +0700

    Add MusverifyEmail

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, MustVerifyEmail;
 
     /**
      * The attributes that are mass assignable.
@@ -56,5 +57,10 @@ class User extends Authenticatable
     public function event()
     {
         return $this->hasMany(Event::class);
+    }
+
+    public function modelActiveEvent()
+    {
+        return $this->hasMany(ModelActiveEvent::class);
     }
 }
