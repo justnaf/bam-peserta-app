5dbcd6374295ba0aadbee016d8786d3bc176a42d
justnaf <naufalaf86@gmail.com>
2025-02-03 17:38:49 +0700

    Modify

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -162,7 +162,7 @@
     <script>
         function sessionDropdown() {
             return {
-                sessions: @json($activeEvent - > event - > sesi)
+                sessions: @json($activeEvent->event->sesi)
                 , groupedSessions: {}
                 , selectedDay: null
                 , selectedTime: null
